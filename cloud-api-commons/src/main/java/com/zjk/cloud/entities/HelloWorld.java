f24be4bf7053259d92d5d058b6db49709d87717d
panda1786291 <46339558+panda1786291@users.noreply.github.com>
2021-11-05 00:23:31 +0800

    Update HelloWorld.java

--- cloud-api-commons/src/main/java/com/zjk/cloud/entities/HelloWorld.java
+++ cloud-api-commons/src/main/java/com/zjk/cloud/entities/HelloWorld.java
@@ -2,6 +2,6 @@ package com.zjk.cloud.entities;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello ");
+        System.out.println("hello world");
     }
 }
